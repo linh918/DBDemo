048bb076c7a3755f8e1f527951604d065ccd49ed
linh918 <duclinhqn1510@gmail.com>
2017-06-30 12:08:07 +0700

    todo: get max id value

---
+++ components/Person.js
@@ -0,0 +1,56 @@
+import Realm from 'realm';
+class Person extends Realm.Object{}
+Person.schema={
+  name:'Person',
+  primaryKey: 'id',
+  properties:{
+    id:'int',
+    name:'string',
+    phone:'int',
+  },
+
+};
+
+
+let realm = new Realm({schema: [ Person]});
+
+export function getPersonList() {
+  return realm.objects('Person').filtered('id>0').sorted('id');
+}
+export function initPersonDatabase() {
+
+  if(realm.objects('Person').length==0){
+    realm.write(() => {
+      realm.create('Person', {
+        id:0,
+        name: 'sdsdsdsd',
+        phone: 0,
+
+      });
+    });
+     console.log("init");
+  }
+ console.log("not init");
+}
+function getNextId(){
+  //   data=getPersonList();
+  //   var person=data[data.length-1];
+  // return (person.id+1);
+}
+export function createPerson(_name, _phone) {
+    var _id=2;
+    var s=  realm.write(() => {
+    realm.create('Person', {
+      id:_id,
+      name: _name,
+      phone: _phone,
+
+    });
+  });
+  console.log(s);
+
+}
+export function deleteAllPerson(){
+  let persons= realm.object('Person');
+  realm.delete(persons);
+}
---
+++ components/PersonListView.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  ListView
+} from 'react-native';
+import { getPersonList, createPerson,initPersonDatabase } from './Person';
+export default class PersonListView extends Component{
+  constructor(props){
+    super(props);
+    // data=[
+    //   { "name":"linh",
+    //     "phone":1234,
+    //
+    //   },
+    //   { "name":"tan",
+    //     "phone":123,
+    //
+    //   },
+    // ];
+   initPersonDatabase();
+  //   createPerson(4,'Linh',123);
+    // //s
+//    createPerson('Linh',123);
+    data=getPersonList();
+    var person=data[data.length-1];
+     console.log(person.id);
+    var ds=new ListView.DataSource({
+      rowHasChanged:(r1,r2)=>r1!==r2}
+    );
+    this.state={
+      dataSource:ds.cloneWithRows(data),
+      };
+  }
+  _renderRow(dataSource){
+    return(
+      <View style={styles.container}>
+         <Text style={styles.title}>{dataSource.name}</Text>
+         <Text style={styles.title}>{dataSource.phone}</Text>
+      </View>
+
+    );
+  }
+
+  render(){
+    return(
+      <View>
+      <ListView
+       dataSource={this.state.dataSource}
+       renderRow={this._renderRow.bind(this)}
+      />
+      </View>
+    );
+  }
+}
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    flexDirection:'row',
+  },
+  image:{
+    flex:4,
+    width:100,
+    height:100,
+  },
+  title:{
+
+    color:'gray',
+    fontSize:14,
+    margin:10,
+
+
+  },
+
+});
